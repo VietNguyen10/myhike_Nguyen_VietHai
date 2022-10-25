3207c6709962c40d66046432fb99e276c451c9d6
Viet Hai Nguyen <vnguyen141@my.bcit.ca>
2022-10-25 15:12:46 -0700

    ,odified by Viet Hai Nguyen, Oct 10, 2022

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,13 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyBbJg0w-2P8Lymk9tJ2hT2ckj3e65s0knQ",
+    authDomain: "comp1800-68813.firebaseapp.com",
+    projectId: "comp1800-68813",
+    storageBucket: "comp1800-68813.appspot.com",
+    messagingSenderId: "402868765863",
+    appId: "1:402868765863:web:b017860ee3baa7caa14782",
+    measurementId: "G-J7FZK500V4"
 };
 
 //--------------------------------------------
